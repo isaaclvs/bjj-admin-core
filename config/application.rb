2645ace9459df328e4860b891add9d0b2271f8c3
Isaac Alves <isaacfelipee3@gmail.com>
2026-04-14 18:32:43 -0300

    feat: i18n PT-BR completo — enums, submit buttons, Devise, e-mails, página 406

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module BjjAdminCore
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml")]
   end
 end
